8d65e092ca615b0b0bd458f3e6f8d6faf2789c39
Graven | Developpement <gravenilvec@gmail.com>
2019-03-03 19:10:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
 
   <title>MaBrancheMasterEstStable</title>
-  <meta name="description" content="Ma branche master est stable konar">
+  <meta name="description" content="Ma branche master est stable ">
   <meta name="author" content="Graven">
 
   <link rel="stylesheet" href="css/styles.css?v=1.0">
