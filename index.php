bbc6af785079eb9d3325051a99e7a48bd21dd080
Arnaud Lier <zeprofdecoding@gmail.com>
2019-03-03 19:09:27 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>MaBrancheMasterEstStable</title>
+  <meta name="description" content="Ma branche master est stable konar">
+  <meta name="author" content="Graven">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
