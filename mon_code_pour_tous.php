e4ee23831fa3c7589aa7950264a0c97cb4bb3de8
Graven | Developpement <gravenilvec@gmail.com>
2019-04-21 10:58:40 +0200

    Delete mon_code_pour_tous.php

--- mon_code_pour_tous.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  echo "Salut à tous c'est Graven et on se retrouve pour une nouvelle vidéo!";
-  echo "Dans cette vidéo, nous allons apprendre a faire du code PHP de kalitay mon khey";
-    
-?>
